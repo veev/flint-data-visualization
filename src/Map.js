168d13a80663729c455e69feae4bc69041faace4
Genevieve Hoffman <genny.hoffman@gmail.com>
2018-01-04 17:10:39 -0800

    getting map to draw full screen

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,9 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import { PropTypes } from 'prop-types'
 import MapboxGl from 'mapbox-gl'
 import { MAPBOX_TOKEN } from './data/keys.js'
 
-Mapboxgl.accessToken = MAPBOX_TOKEN
-
-class Map extends Component {
+export default class Map extends Component {
 	
 	static childContextTypes = {
 		map: PropTypes.object
@@ -19,7 +18,55 @@ class Map extends Component {
   });
 
   componentDidMount() {
+    MapboxGl.accessToken = MAPBOX_TOKEN
 
+    const map = new MapboxGl.Map({
+      container: this.container,
+      style: 'mapbox://styles/mapbox/dark-v9',
+      center: [-83.6969862, 43.0215524],
+      zoom: 12,
+      pitch: 60,
+      bearing: 0
+    });
+
+    map.on('load', (...args) => {
+      this.setState({ map })
+    });
+
+    window.addEventListener('resize', this._resize);
+    this._resize();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (
+      nextProps.children !== this.props.children ||
+      nextState.map !== this.state.map
+    )
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resize);
+  }
+
+  _resize = () => {
+    this.setState({
+      viewport: {
+        ...this.state.viewport,
+        width: this.props.width || window.innerWidth,
+        height: this.props.height || window.innerHeight
+      }
+    });
+  };
+
+  render() {
+    const { children } = this.props;
+    const { map } = this.state;
+
+    return (
+      <div className='Map' ref={(x) => { this.container = x }}>
+        { map && children }
+      </div>
+    )
   }
 
 }
