5e5bb3cfd3c2e233de9f072bcb45d3c75c4eed7d
Genevieve Hoffman <genny.hoffman@gmail.com>
2018-01-06 18:22:53 -0500

    adding sass transpiling

--- src/Controls.js
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react'
-import { PlayButton, PauseButton } from 'react-player-controls'
-
-export default class Controls extends Component {
-  render() {
-    <PlayButton isEnabled={true} onClick={playHandler} />
-    <PauseButton onClick={pauseHandler} />
-  }
-}
