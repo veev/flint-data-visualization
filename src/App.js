cc585abdcdab6b532d8d6621c891e084cb9b3089
Genevieve Hoffman <genny.hoffman@gmail.com>
2018-01-08 15:13:50 -0500

    trying to console lifecycle hooks in main app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 import Map from './Map'
 import Timeline from './Timeline'
 import Title from './Title'
-import TableExampleSelectableInvertedRow from './TableExampleSelectableInvertedRow'
+import Board from './Board'
+
 // import './styles/App.css'
 // import Source from './Source'
 // import Layer from './Layer'
@@ -11,20 +12,39 @@ import TableExampleSelectableInvertedRow from './TableExampleSelectableInvertedR
 import incidents from './data/may5-12-incidents.json'
 
 class App extends Component {
+  constructor() {
+    super(); // for correct context
+    this.state = {
+      isPlaying: false
+    }
+    this.update.bind(this)
+  }
 
-  // state = {
-  //   totalTime: 
-  // };
+  componentWillMount() {
+    // no access yet to component in React DOM (has yet to render)
+    console.log('componentWillMount')
+  }
+
+  update (e) {
+    console.log(e.type)
+    this.setState({currentEvent: e.type})
+  }
 
   render() {
+    console.log('render')
     return (
       <div className="App">
         <Map sourceData={incidents} />
         <Title title="Flint Police Dispatches"/>
-        <Timeline />
-        <TableExampleSelectableInvertedRow />
+        <Timeline onClick={this.update}/>
+        <Board />
+        <h1>{this.state.currentEvent}</h1>
       </div>
     );
+  }
+
+  componentDidMount() {
+    // have access to the component in React DOM (has already rendered)
   }
 }
 
