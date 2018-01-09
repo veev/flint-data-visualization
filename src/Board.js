9718b2b67dae339f4b101c91befa46d2e7e5be25
Genevieve Hoffman <genny.hoffman@gmail.com>
2018-01-08 22:41:19 -0500

    trying to make table data work

--- src/Board.js
+++ src/Board.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react'
 // import TableExampleSelectableInvertedRow from './TableExampleSelectableInvertedRow'
-import { Table } from 'semantic-ui-react'
+import { Table, Sticky } from 'semantic-ui-react'
 
 export default class Board extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {}
   }
 
   componentWillMount() {
     console.log(this.props.sourceData)
   }
 
+  handleContextRef = contextRef => this.setState({ contextRef })
+
   render() {
-    let activeIncidents = this.props.sourceData.features
-    // console.log(data)
+    const activeIncidents = this.props.sourceData.features
+    const { contextRef } = this.state
+
     return(
-      <div className="Board">
-        {activeIncidents.map(activeIncident => {
-          <TableExampleSelectableInvertedRow key={activeIncident.id} incidentInfo={activeIncident}/>
-        })}
+      <div className="Board" ref={this.handleContextRef}>
+        <Table celled selectable collapsing compact>
+          <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Type</Table.HeaderCell>
+                <Table.HeaderCell>Priority</Table.HeaderCell>
+                <Table.HeaderCell>Active Time</Table.HeaderCell>
+              </Table.Row>
+          </Table.Header>
+          <Table.Body>
+          { activeIncidents.map(activeIncident => {
+            // <TableExampleSelectableInvertedRow key={activeIncident.id} incidentInfo={activeIncident}/>
+            return(
+                <Table.Row key={activeIncident.id}>
+                  <Table.Cell>{activeIncident.properties.type}</Table.Cell>
+                  <Table.Cell>{activeIncident.properties.priority}</Table.Cell>
+                  <Table.Cell>{activeIncident.properties.time}</Table.Cell>
+                </Table.Row>
+              ) 
+            })
+          }
+          </Table.Body>
+        </Table>
       </div>
     )
   }
