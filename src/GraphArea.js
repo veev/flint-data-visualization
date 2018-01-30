f062852dc7e807dffdac3ef52409a48680cab508
Genevieve Hoffman <genny.hoffman@gmail.com>
2018-01-29 23:13:37 -0500

    drawing graph in canvas since it's too intense to draw svg

--- src/GraphArea.js
+++ src/GraphArea.js
@@ -25,14 +25,18 @@ export default class GraphArea extends Component {
     };
   }
 
-  // componentDidMount() {
+  componentDidMount() {
+    console.log(this.canvas)
+    const canvas = this.canvas
+    const ctx = canvas.getContext("2d")
+    this.makeCanvasGraph(graphData, ctx)
   //   //console.log(this.props.data)
   //   //this.makeHistogramData(this.props.data.features)
   //   //this.makeIncidentGraph(this.props.data.features)
   //   //this.makeIncidentGraph(graphData)
   //   //this.makeAxis(graphData)
 
-  // }
+  }
 
   shouldComponentUpdate(nextProps, nextState) {
     //console.log("shouldComponentUpdate")
@@ -40,12 +44,15 @@ export default class GraphArea extends Component {
     return true
   }
 
-  // componentDidUpdate() {
+  componentDidUpdate() {
+    const canvas = this.canvas
+    const ctx = canvas.getContext("2d")
+    this.makeCanvasGraph(graphData, ctx)
   //   //this.makeHistogramData(this.props.data.features)
   //   //this.makeIncidentGraph(this.props.data.features)
   //   //this.makeIncidentGraph(graphData)
   //   //this.makeAxis(graphData)
-  // }
+  }
 
   makeAxis = (data) => {
     const dateRange = [data[0].start, data[data.length - 1].end]
@@ -56,6 +63,29 @@ export default class GraphArea extends Component {
                               .domain(dateRange)
 
     const xAxis = axisBottom().scale(xScale)
+
+  }
+
+  makeCanvasGraph = (data, ctx) => {
+
+    const dateRange = [data[0].start, data[data.length - 1].end]
+    const waitRange = extent(data, d => {
+      return d.wait
+    })
+
+    const xScale = scaleTime().range([0, this.props.size[0]])
+                              .domain(dateRange)
+
+    const yScale = scalePow().exponent(0.7)
+                              .range([this.props.size[1] - 45, 0])
+                              .domain(waitRange)
+
+    data.map( (d,i) => {
+      ctx.fillStyle = colrs[1] // sets the color to fill in the rectangle with
+      ctx.fillRect(xScale(d.start), yScale(d.wait), this.getRectWidth(xScale, d, true), 1)
+      ctx.fillStyle = colrs[0]
+      ctx.fillRect(xScale(d.scene), yScale(d.wait), this.getRectWidth(xScale, d, false), 1)
+    })
 
   }
 
@@ -307,47 +337,63 @@ export default class GraphArea extends Component {
 
     const xAxis = axisBottom().scale(xScale)
 
-    const x = this.getScales().xScale
-    const y = this.getScales().yScale
+    // const x = this.getScales().xScale
+    // const y = this.getScales().yScale
 
     return (
-      <svg 
+      <div className="graphWrapper">
+      <canvas width={this.props.size[0]} height={this.props.size[1]} ref={(el) => { this.canvas = el }} />
+      <svg
+        className="svgLayer"
         ref={node => this.node = node}
         width={this.props.size[0]}
         height={this.props.size[1]}>
-        <g className="graph">
-          { graphData.map( (d, i) => {
-            return (
-            <g className="stackGroup" key={`group-${i}`}>
-            <rect
-              key={`total-${i}`}
-              className="totalDuration"
-              x={x(d.start)}
-              y={y(d.wait)}
-              width={this.getRectWidth(x, d, true)}
-              height={1.5}
-              fill={colrs[1]}
-            />
-            <rect 
-              key={`onScene-${i}`}
-              className="onSceneDuration" 
-              x={x(d.scene)}
-              y={y(d.wait)}
-              width={this.getRectWidth(x, d, false)}
-              height={1.5}
-              fill={colrs[0]}
-            />
-          </g>
-          )
-        })}
         <Axis 
           h={this.props.size[1]}
           axis={xAxis}
           axisType="x"
         />
-        </g>
       </svg>
-
+      </div>
     )
+
+    // return (
+    //   <svg 
+    //     ref={node => this.node = node}
+    //     width={this.props.size[0]}
+    //     height={this.props.size[1]}>
+    //     <g className="graph">
+    //       { graphData.map( (d, i) => {
+    //         return (
+    //         <g className="stackGroup" key={`group-${i}`}>
+    //         <rect
+    //           key={`total-${i}`}
+    //           className="totalDuration"
+    //           x={x(d.start)}
+    //           y={y(d.wait)}
+    //           width={this.getRectWidth(x, d, true)}
+    //           height={1.5}
+    //           fill={colrs[1]}
+    //         />
+    //         <rect 
+    //           key={`onScene-${i}`}
+    //           className="onSceneDuration" 
+    //           x={x(d.scene)}
+    //           y={y(d.wait)}
+    //           width={this.getRectWidth(x, d, false)}
+    //           height={1.5}
+    //           fill={colrs[0]}
+    //         />
+    //       </g>
+    //       )
+    //     })}
+    //     <Axis 
+    //       h={this.props.size[1]}
+    //       axis={xAxis}
+    //       axisType="x"
+    //     />
+    //     </g>
+    //   </svg>
+    // )
   }
 }
