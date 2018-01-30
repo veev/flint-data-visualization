a9ae68a368c2a04ee148b3a3437659b4f34f3be7
Genevieve Hoffman <genny.hoffman@gmail.com>
2018-01-29 23:34:23 -0500

    added axis component

---
+++ src/Axis.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { PropTypes } from 'prop-types'
+import { select } from 'd3-selection'
+
+export default class Axis extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  componentDidUpdate() {
+    this.renderAxis()
+  }
+
+  componentDidMount() {
+    this.renderAxis()
+  }
+
+  renderAxis = () => {
+    const node = ReactDOM.findDOMNode(this)
+    select(node).call(this.props.axis)
+  }
+
+  render() {
+    const translate = `translate(0,${this.props.h - 25})`
+
+    return (
+      <g className="axis" transform={this.props.axisType=='x'?translate:""} >
+      </g>
+    )
+  }
+
+}
+
+Axis.propTypes = {
+  h: PropTypes.number,
+  axis: PropTypes.func,
+  axisType: PropTypes.oneOf(['x','y'])
+
+}
