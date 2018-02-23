2e05e8a08272ce141474e5f00609317bf7fcaa7b
Genevieve Hoffman <genny.hoffman@gmail.com>
2018-02-23 13:42:06 -0500

    console logs

--- src/GraphArea.js
+++ src/GraphArea.js
@@ -57,8 +57,8 @@ export default class GraphArea extends Component {
   }
 
   componentWillUpdate(nextProps) {
-    console.log(nextProps.dateRange, this.props.dateRange)
-    console.log(nextProps.dateRange[0] !== this.props.dateRange[0] || nextProps.dateRange[1] !== this.props.dateRange[1])
+    // console.log(nextProps.dateRange, this.props.dateRange)
+    // console.log(nextProps.dateRange[0] !== this.props.dateRange[0] || nextProps.dateRange[1] !== this.props.dateRange[1])
     //return nextProps.data !== this.props.data
     if (nextProps.dateRange[0] !== this.props.dateRange[0] || nextProps.dateRange[1] !== this.props.dateRange[1]) {
       this.bins.length = 0
